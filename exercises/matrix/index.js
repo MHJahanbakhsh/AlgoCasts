b1dc4a95558f381743917384257912265f75ede9
MohammadHossein Jahanbakhsh <m.jahanbakhsh@bluteam.ir>
2024-08-09 10:31:37 +0330

    feat(execises/matrix): just got it one with it! fuuuuck

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -10,11 +10,74 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4],
+//     [
+//     [1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
-//     [10,  9,  8, 7]]
+//     [10,  9,  8, 7]
+//     ]
 
-function matrix(n) {}
+
+
+/*
+ 
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+| 8 | 9 | 4 |
++---+---+---+
+| 7 | 6 | 5 |
++---+---+---+
+
+*/
+
+/*
+Time Complexity: 𝑂(𝑁2)
+Space Complexity: 𝑂(𝑁2)
+*/
+
+function matrix(n) {
+    const ref = []
+    let startRow = 0
+    let startCol = 0
+    let endRow = n - 1
+    let endCol = n - 1
+    let counter = 1
+    for (let i = 1; i <= n; i++) {
+        ref.push([])
+    }
+
+    while (startRow <= endRow && startCol <= endCol) {
+        // Top row
+        for (let i = startCol; i <= endCol; i++) {
+            ref[startRow][i] = counter
+            counter++
+        }
+        startRow++
+
+        // Right column
+        for (let i = startRow; i <= endRow; i++) {
+            ref[i][endCol] = counter
+            counter++
+        }
+        endCol--
+
+        //Bottom Row
+        for (let i = endCol; i >= startCol; --i) {
+            ref[endRow][i] = counter
+            counter++
+        }
+        endRow--
+
+        // Start column
+        for (let i = endRow; i >= startRow; i--) {
+            ref[i][startCol] = counter;
+            counter++
+        }
+        startCol++
+    }
+    return ref
+}
+matrix(4)
 
 module.exports = matrix;
